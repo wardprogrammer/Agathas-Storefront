6d257413b02df0eba2bfe04dd44c40efb3452265
Amit K <wardprogrammer@gmail.com>
2014-10-10 16:48:34 -0500

    Reference Updates.

--- Agathas.Storefront.Infrastructure/Payments/TransactionResult.cs
+++ Agathas.Storefront.Infrastructure/Payments/TransactionResult.cs
@@ -5,6 +5,6 @@
         public string PaymentMerchant { get; set; }
         public bool PaymentOk { get; set; }
         public string PaymentToken { get; set; }
-        public decimal Amount { get; set; } 
+        public decimal Amount { get; set; }
     }
 }
